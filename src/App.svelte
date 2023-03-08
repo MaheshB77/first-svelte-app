f6e329e40e682ccd6046bf524e94a88fa00b5455
MaheshB77 <mbansode7696@gmail.com>
2023-03-08 12:27:26 +0530

    Added reactive statements

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
   import Fundamentals from "./components/Fundamentals.svelte";
+  import ReactiveStatements from "./components/ReactiveStatements.svelte";
 </script>
 
-<Fundamentals/>
+<!-- <Fundamentals/> -->
+
+<ReactiveStatements/>
---
+++ src/components/ReactiveStatements.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    let counter: number = 0;
+    const updateCounter = () => {
+        counter ++;
+    }
+
+    $: {
+        console.log("Counter changed ! ", counter);
+    }
+</script>
+
+<div class="">
+    <p>Counter :: {counter}</p>
+    <button on:click={updateCounter}>+</button>
+</div>
+
+<style>
+
+</style>
