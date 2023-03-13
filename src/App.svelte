ddd55a9cc3c6a6c4519e2b9470d67f2d29bf55b7
MaheshB77 <mbansode7696@gmail.com>
2023-03-13 15:00:54 +0530

    Updated the folder structure

--- src/App.svelte
+++ src/App.svelte
@@ -5,8 +5,8 @@
   // import Parent from "./components/events/event-forwarding/Parent.svelte";
   import Parent from "./components/events/event-forwarding/shorthand/Parent.svelte"; // Event forwarding shorthand
   import EventDispatcher from "./components/events/EventDispatcher.svelte";
-  import Fundamentals from "./components/Fundamentals.svelte";
-  import ReactiveStatements from "./components/ReactiveStatements.svelte";
+  import Fundamentals from "./components/fundamentals/Fundamentals.svelte";
+  import ReactiveStatements from "./components/reactive-statements/ReactiveStatements.svelte";
 </script>
 
 <!-- <Fundamentals/> -->
